dcf70c376dd3c1c797e2fb1797fbc4b57fb00420
QCPOLstakepool <qcpolstakepool@gmail.com>
2024-08-28 11:32:36 -0400

    Refactoring

--- src/fortuna_stream_sinks/HttpRequestHandler.py
+++ src/fortuna_stream_sinks/HttpRequestHandler.py
@@ -3,7 +3,6 @@ import logging
 import os
 import base64
 import binascii
-import sys
 from http.server import BaseHTTPRequestHandler
 from TwitterAPI import TwitterAPI
 from pycardano import VerificationKeyHash, Address, Network
@@ -19,87 +18,136 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
     x_api = TwitterAPI(X_API_KEY, X_API_KEY_SECRET, X_ACCESS_TOKEN, X_ACCESS_TOKEN_SECRET, api_version="2")
 
     def do_POST(self):
-        if self.path == "/api/mints":
-            self.post_mints()
+        if self.path == "/api/events":
+            content_length = int(self.headers.get('Content-Length'))
+            post_body_json = json.loads(self.rfile.read(content_length))
+
+            HttpRequestHandler.logger.debug(post_body_json)
+
+            if HttpRequestHandler.is_mint(post_body_json):
+                HttpRequestHandler.post_mint(post_body_json)
+
+                self.created()
+            elif HttpRequestHandler.is_conversion(post_body_json):
+                HttpRequestHandler.post_conversion(post_body_json)
+
+                self.created()
+            else:
+                self.no_content()
         else:
             self.not_found()
 
-    def post_mints(self):
-        content_length = int(self.headers.get('Content-Length'))
-        post_body_json = json.loads(self.rfile.read(content_length))
+    @staticmethod
+    def is_mint(post_body_json) -> bool:
+        if "outputs" not in post_body_json:
+            return False
 
-        HttpRequestHandler.logger.debug(post_body_json)
+        outputs_datum = list(filter(lambda output: "datumHash" in output, post_body_json["outputs"]))
 
-        address = None
-        mint_asset_amount = -1
-        block_number = -1
-        leading_zeroes = -1
-        difficulty = -1
+        if len(outputs_datum) != 1:
+            return False
 
-        if "mint" in post_body_json:
-            mint_assets_policy = list(filter(lambda mint: mint["policyId"] == "yYH8mOdh47tErjXn2XrmIn9oS8tvUKY2dT2kjg==", post_body_json["mint"]))  # TODO decode
+        output_datum = outputs_datum[0]
 
-            if len(mint_assets_policy) == 1:
-                mint_assets = list(filter(lambda mint: mint["name"] == "VFVOQQ==", mint_assets_policy[0]["assets"]))  # TODO decode
+        if "mint" not in post_body_json:
+            return False
 
-                if len(mint_assets) == 1:
-                    mint_asset_amount = int(mint_assets[0]["mintCoin"])
+        mint_assets_policy = list(filter(lambda mint: mint["policyId"] == "yYH8mOdh47tErjXn2XrmIn9oS8tvUKY2dT2kjg==", post_body_json["mint"]))  # TODO decode to asset1up3fehe0dwpuj4awgcuvl0348vnsexd573fjgq
 
-        if "outputs" in post_body_json:
-            outputs = list(filter(lambda output: "assets" in output, post_body_json["outputs"]))
+        if len(mint_assets_policy) != 1 or "mintCoin" not in mint_assets_policy[0]:
+            return False
 
-            for output in outputs:
-                outputs_assets = list(filter(lambda _output: "assets" in _output, output["assets"]))
+        return "constr" in output_datum and \
+            "fields" in output_datum["constr"] and \
+            len(output_datum["constr"]["fields"]) == 7 and \
+            "bigInt" in output_datum["constr"]["fields"][0] and \
+            "int" in output_datum["constr"]["fields"][0]["bigInt"] and \
+            "boundedBytes" in output_datum["constr"]["fields"][1] and \
+            "bigInt" in output_datum["constr"]["fields"][2] and \
+            "int" in output_datum["constr"]["fields"][2]["bigInt"] and \
+            "bigInt" in output_datum["constr"]["fields"][3] and \
+            "int" in output_datum["constr"]["fields"][3]["bigInt"] and \
+            "bigInt" in output_datum["constr"]["fields"][4] and \
+            "int" in output_datum["constr"]["fields"][4]["bigInt"] and \
+            "bigInt" in output_datum["constr"]["fields"][5] and \
+            "int" in output_datum["constr"]["fields"][5]["bigInt"] and \
+            "boundedBytes" in output_datum["constr"]["fields"][6]
 
-                for outputs_asset in outputs_assets:
-                    outputs_assets_assets = list(filter(lambda _output: "name" in _output and _output["name"] == "VFVOQQ==", outputs_asset["assets"]))
+    @staticmethod
+    def is_conversion(post_body_json) -> bool:
+        return False
 
-                    if len(outputs_assets_assets) == 1:
-                        address_hex = binascii.hexlify(base64.b64decode(output["address"])).decode()
+    @staticmethod
+    def post_mint(post_body_json):
+        address = HttpRequestHandler.get_mint_miner_address(post_body_json)
+        mint_asset_amount = HttpRequestHandler.get_mint_amount(post_body_json)
+        block_number, leading_zeroes, difficulty = HttpRequestHandler.get_mint_data(post_body_json)
 
-                        if len(address_hex) == 114:
-                            payment_hash = VerificationKeyHash(bytes.fromhex(address_hex[2:58]))
-                            stake_hash = VerificationKeyHash(bytes.fromhex(address_hex[58:]))
-                            address = Address(payment_part=payment_hash, staking_part=stake_hash, network=Network.MAINNET).encode()
-                        elif len(address_hex) == 58:
-                            payment_hash = VerificationKeyHash(bytes.fromhex(address_hex[2:]))
-                            address = Address(payment_part=payment_hash, network=Network.MAINNET).encode()
+        with open("pools.json", "r") as pools_file:
+            pools = json.load(pools_file)
 
-                        break
+        pool = list(filter(lambda _pool: _pool["address"] == address, pools))
 
-                if address is not None:
-                    break
-
-            outputs_datum = list(filter(lambda output: "datumHash" in output, post_body_json["outputs"]))
-
-            if len(outputs_datum) == 1:
-                if "datum" in outputs_datum[0] and "constr" in outputs_datum[0]["datum"] and "fields" in outputs_datum[0]["datum"]["constr"] and "bigInt" in outputs_datum[0]["datum"]["constr"]["fields"][0]:
-                    block_number = int(outputs_datum[0]["datum"]["constr"]["fields"][0]["bigInt"]["int"])
-                    leading_zeroes = int(outputs_datum[0]["datum"]["constr"]["fields"][3]["bigInt"]["int"])
-                    difficulty = int(outputs_datum[0]["datum"]["constr"]["fields"][4]["bigInt"]["int"])
-
-        if mint_asset_amount > 0 and block_number > 0:
-            with open("pools.json", "r") as pools_file:
-                pools = json.load(pools_file)
-
-            pool = list(filter(lambda _pool: _pool["address"] == address, pools))
-
-            if len(pool) == 1:
-                message = f"Block {block_number} mined by pool {pool[0]["name"]} ({pool[0]["website"]}) (rewards: {mint_asset_amount / 100000000})."
-            else:
-                message = f"Block {block_number} mined by solo miner {address[:12]}...{address[-4:]} (rewards: {mint_asset_amount / 100000000})."
-
-            HttpRequestHandler.logger.info(message)
-
-            x_response = HttpRequestHandler.x_api.request("tweets", {"text": message}, method_override="POST")
-
-            HttpRequestHandler.logger.debug(f"X response: {x_response.text}")
-
-            self.send_response(201)
-            self.end_headers()
+        if len(pool) == 1:
+            message = f"Block {block_number} mined by pool {pool[0]["name"]} ({pool[0]["website"]}) (rewards: {mint_asset_amount / 100000000})."
         else:
-            self.send_response(204)
-            self.end_headers()
+            message = f"Block {block_number} mined by solo miner {address[:12]}...{address[-4:]} (rewards: {mint_asset_amount / 100000000})."
+
+        HttpRequestHandler.logger.info(message)
+
+        x_response = HttpRequestHandler.x_api.request("tweets", {"text": message}, method_override="POST")
+
+        HttpRequestHandler.logger.debug(f"X response: {x_response.text}")
+
+    @staticmethod
+    def post_conversion(post_body_json):
+        pass
+
+    @staticmethod
+    def get_mint_miner_address(post_body_json) -> str:
+        outputs = list(filter(lambda output: "assets" in output, post_body_json["outputs"]))
+
+        for output in outputs:
+            outputs_assets = list(filter(lambda _output: "assets" in _output, output["assets"]))
+
+            for outputs_asset in outputs_assets:
+                outputs_assets_assets = list(filter(lambda _output: "name" in _output and _output["name"] == "VFVOQQ==", outputs_asset["assets"]))
+
+                if len(outputs_assets_assets) == 1:
+                    address_hex = binascii.hexlify(base64.b64decode(output["address"])).decode()
+
+                    if len(address_hex) == 114:
+                        payment_hash = VerificationKeyHash(bytes.fromhex(address_hex[2:58]))
+                        stake_hash = VerificationKeyHash(bytes.fromhex(address_hex[58:]))
+                        return Address(payment_part=payment_hash, staking_part=stake_hash, network=Network.MAINNET).encode()
+                    elif len(address_hex) == 58:
+                        payment_hash = VerificationKeyHash(bytes.fromhex(address_hex[2:]))
+                        return Address(payment_part=payment_hash, network=Network.MAINNET).encode()
+
+        return "unknown address"
+
+    @staticmethod
+    def get_mint_amount(post_body_json) -> int:
+        mint_assets_policy = list(filter(lambda mint: mint["policyId"] == "yYH8mOdh47tErjXn2XrmIn9oS8tvUKY2dT2kjg==", post_body_json["mint"]))  # TODO decode to asset1up3fehe0dwpuj4awgcuvl0348vnsexd573fjgq
+        mint_assets = list(filter(lambda mint: mint["name"] == "VFVOQQ==", mint_assets_policy[0]["assets"]))  # TODO decode
+
+        return int(mint_assets[0]["mintCoin"])
+
+    @staticmethod
+    def get_mint_data(post_body_json) -> (int, int, int):
+        outputs_datum = list(filter(lambda output: "datumHash" in output, post_body_json["outputs"]))
+
+        return int(outputs_datum[0]["datum"]["constr"]["fields"][0]["bigInt"]["int"]), \
+            int(outputs_datum[0]["datum"]["constr"]["fields"][3]["bigInt"]["int"]), \
+            int(outputs_datum[0]["datum"]["constr"]["fields"][4]["bigInt"]["int"])
+
+    def created(self):
+        self.send_response(201)
+        self.end_headers()
+
+    def no_content(self):
+        self.send_response(204)
+        self.end_headers()
 
     def not_found(self):
         self.send_response(404)
