ec06538a701ffd86b306563c20692680a061fd5f
QCPOLstakepool <qcpolstakepool@gmail.com>
2024-08-28 10:21:59 -0400

    logs

--- src/fortuna_stream_sinks/Process.py
+++ src/fortuna_stream_sinks/Process.py
@@ -2,7 +2,7 @@ import subprocess
 
 class Process:
     @staticmethod
-    def run(process: str, input=None) -> str:
+    def run(process: list[str], input=None) -> str:
         completed_command = subprocess.run(process, capture_output=True, text=True, input=input)
 
         if not completed_command.returncode == 0:
